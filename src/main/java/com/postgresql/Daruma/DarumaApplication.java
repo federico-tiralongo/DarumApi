16c15c277d52fcb7c9c92ae5ea2f8451f17d41c7
RafaelCaballeroMora <rafaelcaballeromora@gmail.com>
2024-05-06 14:22:47 +0100

    add DB startup scripts

--- src/main/java/com/postgresql/Daruma/DarumaApplication.java
+++ src/main/java/com/postgresql/Daruma/DarumaApplication.java
@@ -2,12 +2,8 @@ package com.postgresql.Daruma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
-@EnableJpaRepositories
-// This anotation allos running without a datasource running. Uncomment it if
-// you with to deploy locally without deploying postgresql
 public class DarumaApplication {
 
 	public static void main(String[] args) {
